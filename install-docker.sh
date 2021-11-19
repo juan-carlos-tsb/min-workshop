652631c338318a310faef16cb770233f0eccfecb
Juan Carlos Carvajal Bermudez <jccb@pop-os>
2021-11-19 15:13:17 +0100

    Update install-docker.sh

--- install-docker.sh
+++ install-docker.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+apt-get install ca-certificates curl gnupg lsb-release
+curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt-get update
 apt-get install docker-ce docker-ce-cli containerd.io
 docker volume create portainer_data
